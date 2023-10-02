46473436cff38e9167153e554780a81f67d16aed
MRajczyk <mikolajrajczyk01@gmail.com>
2023-10-02 16:40:59 +0200

    connecting and sending msg to mqtt broker works

---
+++ 5 ESP connection to MQTT broker (hardcodeded)/espcode/espcode/espcode.ino
@@ -0,0 +1,116 @@
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+
+/****** WiFi Connection Details *******/
+const char* ssid = "xxxx";
+const char* password = "xxxx";
+
+/******* MQTT Broker Connection Details *******/
+// const char* mqtt_server = "192.168.100.3";
+IPAddress mqtt_server = {192, 168, 100, 3};
+const char* mqtt_username = "esp8266";
+const char* mqtt_password = "password";
+const int mqtt_port = 1883;
+
+/**** WiFi Connectivity Initialisation *****/
+WiFiClient espClient;
+
+/**** MQTT Client Initialisation Using WiFi Connection *****/
+PubSubClient client(espClient);
+
+String macToStr(const uint8_t* mac){
+
+  String result;
+
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+
+    if (i < 5){
+      result += ':';
+    }
+  }
+
+  return result;
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("\nWiFi connected\nIP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+/************* Connect to MQTT Broker ***********/
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientName = "esp8266-";
+    uint8_t mac[6];
+    WiFi.macAddress(mac);
+    clientName += macToStr(mac);
+    Serial.print("Client ID : ");
+    Serial.println(clientName);
+    // Attempt to connect
+    if (client.connect(clientName.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+
+      client.subscribe("led_state");   // subscribe the topics here
+
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");   // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String incommingMessage = "";
+  for (int i = 0; i < length; i++) incommingMessage+=(char)payload[i];
+
+  Serial.println("Message arrived ["+String(topic)+"]"+incommingMessage);
+}
+
+/**** Method for Publishing MQTT Messages **********/
+void publishMessage(const char* topic, String payload , boolean retained){
+  if (client.publish(topic, payload.c_str(), true))
+      Serial.println("Message published ["+String(topic)+"]: "+payload);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial) delay(1);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) reconnect(); // check if client is connected
+  client.loop();
+  DynamicJsonDocument doc(1024);
+
+  doc["deviceId"] = "NodeMCU";
+  doc["test"] = "test";
+
+  char mqtt_message[128];
+  serializeJson(doc, mqtt_message);
+
+  publishMessage("esp8266_data", mqtt_message, true);
+
+  delay(5000);
+}
